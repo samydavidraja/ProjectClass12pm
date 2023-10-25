f29bcfd604695f3d92593f2943c8cf530792d312
samydavidraja <samydavidraja@gmail.com>
2023-10-25 14:36:59 +0530

    Added 3 new methods from Red

--- src/test/java/com/sample/ClassA.java
+++ src/test/java/com/sample/ClassA.java
@@ -17,4 +17,19 @@ public class ClassA {
 
 	}
 
+	private void tamilNadu() {
+		System.out.println("Tamil");
+
+	}
+
+	private void kerala() {
+		System.out.println("Malayalam");
+
+	}
+
+	private void andhraPradesh() {
+		System.out.println("Telugu");
+
+	}
+
 }
